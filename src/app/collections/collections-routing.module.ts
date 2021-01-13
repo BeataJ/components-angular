10c38e2c71ee030bd89e17aa4664cc60d70b25bc
beata <beata@emc2wave.com>
2021-01-13 15:01:48 -0800

    import 3 components to router file

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CampaniesComponent } from './campanies/campanies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CollectionsHomeComponent,
+    children: [],
   },
 ];
 
