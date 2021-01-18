e271d9770c9afddd4cb25864c4ef06aecde51bd9
beata <beata@emc2wave.com>
2021-01-17 20:36:57 -0800

    generate Modal component

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { ModalComponent } from './modal/modal.component';
 
 
 @NgModule({
-  declarations: [ModsHomeComponent],
+  declarations: [ModsHomeComponent, ModalComponent],
   imports: [
     CommonModule,
     ModsRoutingModule
