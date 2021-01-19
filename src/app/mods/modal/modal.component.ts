99435e9c8fdde8c184f536caae6e8ed5cefa15d9
beata <beata@emc2wave.com>
2021-01-18 19:05:26 -0800

    add onDestroy

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
   }
 }
