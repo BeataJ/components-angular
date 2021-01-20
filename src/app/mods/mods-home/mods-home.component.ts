0c25dc22869a582a574ae3a402c6d33e3c30816a
beata <beata@emc2wave.com>
2021-01-19 19:08:25 -0800

    create items array

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items: any = [
+    {
+      title: 'Why is the sky blue?',
+      content: 'The sky is blue because it is',
+    },
+    {
+      title: 'What does an orange taste like?',
+      content: 'An orange tastes like an orange',
+    },
+    {
+      title: 'Why color is that cat?',
+      content: 'The cat is an orange color',
+    },
+  ];
 
   constructor() {}
 
